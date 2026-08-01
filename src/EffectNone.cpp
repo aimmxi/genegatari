6f1560b5adcbfb4135885f4efb38b7c28ef97ada
AIMMXI <aimmxi@aimmxi.xyz>
2026-08-01 23:30:19 +0200

    Fixed flickering bug on ImGUI windows

--- src/EffectNone.cpp
+++ src/EffectNone.cpp
@@ -11,9 +11,6 @@ void EffectNone::render() {
     // Set the background color and clear the previous buffer
     glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
-
-    // Swap buffers
-    SDL_GL_SwapWindow(SDL_GetWindowFromID(1));
 }
 
 // Override
